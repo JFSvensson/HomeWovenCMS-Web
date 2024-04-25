3a7fa0e7a02b199c85e57d7b8383325a1e6747a8
Fredrik Svensson <fs222id@student.lnu.se>
2024-04-25 21:33:57 +0200

    Update register page component with UI changes and imports

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,25 @@
+import React, { useState } from 'react'
+import Button from '@/components/ui/button'
+import { RegisterForm } from '@/components/ui/register-form'
+
 export default function Home() {
   return (
     <main>
-      Register
+      <h2>Register</h2>
+      <p>Register an account to start using HomeWoven CMS</p>
+        {/* <Button
+          buttonText="Register account"
+          className="primary"
+          onClick={handleRegisterClick}
+        />
+        <p>Already registered?
+          <Button
+            buttonText="Log in here!"
+            className="link"
+            onClick={handleLoginClick}
+            />
+        </p> */}
+        <RegisterForm />
     </main>
   )
 }
