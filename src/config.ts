d9283710d5378a56b5e8830d83629870470c16c9
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-02 12:02:53 +0200

    Update API base URL in config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Define the API base URL
 // Development
-export const HOMEWOVEN_API_URL = 'http://localhost:8080/api/v1'
+// export const HOMEWOVEN_API_URL = 'http://localhost:8080/api/v1'
 
 // Production
-// export const HOMEWOVEN_API_URL = 'http://.../api/v1'
+export const HOMEWOVEN_API_URL = 'https://svenssonom.se/homewovencms/api/v1'
