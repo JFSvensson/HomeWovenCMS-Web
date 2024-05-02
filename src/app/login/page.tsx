1355d66ef6abdec755ee6383ae614cc8df85bc9b
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-02 23:34:01 +0200

    Refactor login page to redirect to dashboard if user is authenticated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,12 +2,19 @@
 
 import React from 'react'
 import { LoginForm } from '@/components/ui/login-form'
+import { useAuth } from '@/context/AuthContext'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
-
-  return (
-    <main>
-        <LoginForm />
-    </main>
-  )
+  const { user } = useAuth()
+  const router = useRouter()
+  console.log('User: ', user)
+  if (user) {
+    router.push('/dashboard')
+  } else {
+    return (
+      <main>
+          <LoginForm />
+      </main>
+  )}
 }
