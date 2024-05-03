b3148afca8abc74554f4d0f7bf025083fc796204
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-03 09:14:28 +0200

    Cleanup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,7 +8,6 @@ import { useRouter } from 'next/navigation'
 export default function Home() {
   const { user } = useAuth()
   const router = useRouter()
-  console.log('User: ', user)
   if (user) {
     router.push('/dashboard')
   } else {
