93348edeaf5b947f7bc487eb4655b547739ac805
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-09 13:08:29 +0200

    Fix side effect error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { LoginForm } from '@/components/ui/login-form'
 import { useAuth } from '@/context/AuthContext'
 import { useRouter } from 'next/navigation'
@@ -8,12 +8,20 @@ import { useRouter } from 'next/navigation'
 export default function Home() {
   const { user } = useAuth()
   const router = useRouter()
-  if (user) {
-    router.push('/dashboard')
-  } else {
+  
+  useEffect(() => {
+    if (user) {
+      router.push('/dashboard')
+    }
+  }, [user, router])
+
+  if (!user) {
     return (
       <main>
           <LoginForm />
       </main>
-  )}
+    )
+  }
+
+  return null
 }
