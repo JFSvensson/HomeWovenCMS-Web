5cfc182da128f7b2146fcd65ac0f94ab9f2fafc7
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-11 14:04:26 +0200

    Refactor NewsPage to remove double code

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,50 +1,28 @@
 'use client'
 
-import React, { createContext, useState, useEffect, ReactNode } from 'react'
+import React, { createContext, useState, useEffect, ReactNode, useContext } from 'react'
 import Image from 'next/image'
 import { Article } from '@/types/ArticleTypes'
 
 export default function NewsPage() {
 
   const [articles, setArticles] = useState<Article[] | null>(null)
-  
-  const fetchArticles = async () => {
-    const API_ARTICLE_URL = 'https://svenssonom.se/homewovencms/api/v1/articles'
-
-    try {
-      const response = await fetch(API_ARTICLE_URL, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
-        }
-      })
-      const data = await response.json()
-      console.log(data)
-      setArticles(data)
-    } catch (error) {
-      console.error('Failed to fetch articles:', error)
-    }
-  }
-  
-  useEffect(() => {
-    fetchArticles()
-  }, [])
-  
 
   return (
-      <div  className='bg-white text-black p-6 rounded shadow-md'>
+      <div className='bg-white text-black p-6 rounded shadow-md'>
         <ul>
         {articles && Object.values(articles)
             .filter((article) => article.id !== undefined)
+            .reverse()
             .map((article) => (
               <li key={article.id}>
               <div>
-                {article.title}
+                <strong>{article.title}</strong>
                 {/* <Image src={article.imageUrl} alt={article.imageText} width="100" height="100" /> */}
                 <Image src="/TestImage.jpg" alt="Freja walking in forest." width={100} height={100} />
-                <div>
-                </div>
+                {article.body}
+                <br />
+                <hr></hr>
               </div>
             </li>
           ))}
