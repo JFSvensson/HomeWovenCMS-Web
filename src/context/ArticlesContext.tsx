41ba4316f3caf4b8a029d5b3e80763d5761c5da4
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-11 13:58:37 +0200

    Add ArticlesContext and ArticlesProvider components

---
+++ src/context/ArticlesContext.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import React, { createContext, useState, useEffect, ReactNode, useContext } from 'react'
+import { Article } from '@/types/ArticleTypes'
+
+interface Props {
+  children: ReactNode
+}
+
+type ArticlesContextType = {
+  articles: Article[]
+  fetchAllArticles: () => void
+}
+
+const defaultArticleValue: ArticlesContextType = {
+  articles: [],
+  fetchAllArticles: () => {},
+}
+
+export const ArticlesContext = React.createContext<ArticlesContextType>(defaultArticleValue)
+
+export const ArticlesProvider: React.FC<Props> = ({ children }) => {
+  const [articles, setArticles] = useState([])
+
+  const fetchAllArticles = async () => {
+    const API_ARTICLE_URL = 'https://svenssonom.se/homewovencms/api/v1/articles'
+
+    try {
+      const response = await fetch(API_ARTICLE_URL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+      })
+      const data = await response.json()
+      console.log(data)
+      setArticles(data)
+    } catch (error) {
+      console.error('Failed to fetch articles:', error)
+    }
+  }
+
+  const debounce = (func: any, delay: any) => {
+    let debounceTimer: any
+    return function(this: any) {
+      const context: any = this
+      const args = arguments
+      clearTimeout(debounceTimer)
+      debounceTimer = setTimeout(() => func.apply(context, args), delay)
+    }
+  }
+
+  const debouncedFetchAllArticles = debounce(fetchAllArticles, 2000)
+
+  useEffect(() => {
+    debouncedFetchAllArticles()
+  })
+
+  return (
+    <ArticlesContext.Provider value={{ articles, fetchAllArticles }}>
+      {children}
+    </ArticlesContext.Provider>
+  )
+}
+
+export const useArticles = () => useContext(ArticlesContext)
