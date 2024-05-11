2a505ed432576029483b3aab4b760af7d6070012
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-11 13:51:39 +0200

    Fix error being routed to dashboard with expired token

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { createContext, useContext, useState, useEffect, ReactNode } from 'react'
+import jwt from 'jsonwebtoken'
 import { AuthContextType, User } from '@/types/AuthTypes'
 import { login as loginUser, logout as logoutUser, fetchUserInformation } from '../app/actions/auth'
 
@@ -23,9 +24,14 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     const token = localStorage.getItem('accessToken')
   
     if (token) {
-      // If the token exists, use it to fetch the user's information and set the user state.
-      const username = fetchUserInformation(token)
-      setUser({ username })
+      const decodedToken = jwt.decode(token)
+      const currentTime = Date.now() / 1000
+
+      if (decodedToken && typeof decodedToken !== 'string' && decodedToken.exp && decodedToken.exp > currentTime) {
+        // If the token exists and is not expired, use it to fetch the user's information and set the user state.
+        const username = fetchUserInformation(token)
+        setUser({ username })
+      }
     }
   }, [])
 
